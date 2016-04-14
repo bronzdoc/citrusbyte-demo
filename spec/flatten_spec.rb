9e8e26c190edd9bdacadc1cf6299b092fe0dd2d7
bronzdoc <lsagastume1990@gmail.com>
2016-04-13 22:19:40 -0600

    Add trickier test for Array#custom_flatten

--- spec/flatten_spec.rb
+++ spec/flatten_spec.rb
@@ -1,8 +1,13 @@
+require "flatten"
+
 RSpec.describe Array do
   describe "#custom_flatten" do
     it "should flatten the array" do
       a = [[1,2,[3]],4]
+      b = [[1], 2, [[3, 4], 5], [[[]]], [[[6]]], 7, 8, []]
+
       expect(a.custom_flatten).to eq([1, 2, 3, 4])
+      expect(b.custom_flatten).to eq([1, 2, 3, 4, 5, 6, 7, 8])
     end
   end
 end
