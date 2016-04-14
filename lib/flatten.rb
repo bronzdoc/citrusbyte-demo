581eb2e88e07b0978da9afb8394502f5e471a02a
bronzdoc <lsagastume1990@gmail.com>
2016-04-13 22:50:42 -0600

    Clean a little bit

--- lib/flatten.rb
+++ lib/flatten.rb
@@ -1,17 +1,10 @@
-#a = [[1,2,[3]],4]
-#a.custom_flatten
-
-#-> [1,2,[3]]
-#-> []
 class Array
-  attr_reader :me
 
+  # Returns a flatten copy of self
   def custom_flatten
-    flat_array = []
     @me ||= self.dup
 
-    return if @me.nil?
-
+    flat_array = []
     @me.each do |val|
       if val.is_a?(Array)
         @me = val
